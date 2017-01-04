5a3fdf0a7715238fe5afe2486bd9e66507591757
NK <test@tes.tu>
2017-01-04 17:50:38 +0000

    Some Fixes

--- program/client.py
+++ program/client.py
@@ -6,16 +6,16 @@ import socket
 import sys
 
 
-result = stun.get_ip_info(source_ip=SOURCE_HOST,
-                          source_port=SOURCE_PORT,
-                          stun_host=stun.STUN_SERVERS[0]);
-OWN_EXTERNAL_IP = result.external_ip;
-OWN_EXTERNAL_PORT = result.external_port;
+#result = stun.get_ip_info(source_ip=SOURCE_HOST,
+#                          source_port=SOURCE_PORT,
+#                          stun_host=stun.STUN_SERVERS[0]);
+#OWN_EXTERNAL_IP = result.external_ip;
+#OWN_EXTERNAL_PORT = result.external_port;
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM);
 sock.bind((SOURCE_HOST,SOURCE_PORT));
 while 1:
-    sock.send('MSM',(REMOTE_HOST , REMOTE_POR))
+    sock.send('MSM',(REMOTE_HOST , REMOTE_PORT))
     conn, addr = sock.recvfrom(1024)
     print('client addr: ', (addr,conn))
     time.delay(1);
--- program/sub_tools/externalvars.py
+++ program/sub_tools/externalvars.py
@@ -4,7 +4,7 @@ args=getargs();
 SOURCE_PORT = args.sourceport
 SOURCE_HOST = args.sourcehost
 REMOTE_HOST = args.remotehost
-REMOTE_POR = args.remoteport
+REMOTE_PORT = args.remoteport
 OWN_EXTERNAL_IP = ''
 OWN_EXTERNAL_PORT=0
 
